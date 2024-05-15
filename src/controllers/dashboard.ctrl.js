8eb3f34a3e1d960c9b3de9a5e2df11e425cdd068
Walter Frederick Seballos <wolt45@yahoo.com>
2024-05-15 09:06:38 +0800

    Update dashboard.ctrl.js

--- src/controllers/dashboard.ctrl.js
+++ src/controllers/dashboard.ctrl.js
@@ -139,7 +139,7 @@ app.controller(
 
 
     $scope.queAction = function (rid, stts) {
-      if (confirm("Proceed #" + rid + "? ")) {
+      if (confirm("Done  with Appointment? #" + rid + "? ")) {
         $scope.$emit('LOAD');
 
         $rest
